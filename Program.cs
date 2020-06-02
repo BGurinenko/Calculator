de1d252f724f9a905dc7f1d51a0bccc23d2cccad
Никита Гуриненко <bgurinenko@gmail.com>
2020-06-02 19:27:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            result();
+            Console.ReadKey();
+        }
+
+        static void result()
+        {
+            Calc Calculator = new Calc();
+            Console.WriteLine("Введите выражение:");
+            string regul = Console.ReadLine();
+            Console.WriteLine($"Результат: {Calculator.Calculate(regul)}");
+            result();
+        }
+    }
+}
